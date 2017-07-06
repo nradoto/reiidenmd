e7509d2d49715a74dcfa732293c9adbd1793eda7
spaztron64 <mensasses30@gmail.com>
2017-07-06 12:02:02 +0200

    Exit options menu. Add single-track sound test

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "genesis.h"
 
 #include "resources.h"
+#include "sound.h"
 
 fix32 movx;
 fix32 movy;
@@ -24,23 +25,15 @@ void mainmenu()
 					VDP_drawText("Options",15,20);
 					VDP_drawText("Exit",15,21);
 					VDP_drawText("1995 ZUNSoft, 2017 Spaztron64", 5, 26);
-					if (ypos>21 && options == 0)
-					{
-						ypos=ypos-1;
-					}
+					//if (ypos>21 && options == 0)
+					//{
+					//	ypos=ypos-1;
+					//}
 
 		}
 }
-
-void myJoyHandler( u16 joy, u16 changed, u16 state)
+void optionsmenu()
 {
-	if (joy == JOY_1)
-	{
-		if (state & BUTTON_START)
-		{
-			VDP_drawText("player 1 pressed START button ", 0, 0);
-			if (ypos==20)
-			{
 						options=1;
 							VDP_clearTextArea(15, 18, 10, 7);
 							VDP_clearTextArea(15, 18, 10, 7);
@@ -50,17 +43,32 @@ void myJoyHandler( u16 joy, u16 changed, u16 state)
 								VDP_drawText("Lives",15,20);
 								VDP_drawText("Sound Test",15,21);
 								VDP_drawText("Quit",15,22);
-								if (ypos<18)
-								{
-									ypos=ypos+1;
-								}
-								if (ypos>22 && options == 1)
-								{
-									ypos=ypos-1;
-								}
+								//if (ypos<18)
+							//	{
+							//		ypos=ypos+1;
+								//}
+								//if (ypos>22 && options == 1)
+								//{
+								//	ypos=ypos-1;
+								//}
 			}
-			if (ypos==22 && options==1)
+
+void myJoyHandler( u16 joy, u16 changed, u16 state)
+{
+	if (joy == JOY_1)
+	{
+		if (state & BUTTON_START)
+		{
+			VDP_drawText("player 1 pressed START button ", 0, 0);
+			if (ypos==20)
+			optionsmenu();
+			else if (ypos==22 && options==1)
+			options=0;
 			mainmenu();
+			if (ypos==21 && options==1)
+			{
+			SND_startPlay_XGM(angel);
+			}
 		}
 		else if (changed & BUTTON_START)
 		{
@@ -100,9 +108,12 @@ void screen()
 
 int main()
 {
+	VDP_setScreenWidth320();
+	VDP_setScreenHeight240();
 	JOY_init();
 	JOY_setEventHandler( &myJoyHandler );
 	screen();
+	mainmenu();
 
 	while(1)
 	{
@@ -114,7 +125,11 @@ int main()
 							{
 								ypos=ypos-1;
 							}
-						mainmenu();
+							if (ypos>21 && options == 0)
+							{
+								ypos=ypos-1;
+							}
+
 
 		//wait for screen refresh
 		VDP_waitVSync();
